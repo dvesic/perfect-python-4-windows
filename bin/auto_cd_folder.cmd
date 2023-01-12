804e927a4e01ea65cb46f92d172a7eb9a6c49ed6
dvesic <Dejan@Vesic.Org>
2023-01-12 18:16:07 +0100

    Added batch file for auto-cd batch creation

---
+++ bin/auto_cd_folder.cmd
@@ -0,0 +1,30 @@
+@ECHO OFF
+IF NOT DEFINED CONDA_DEFAULT_ENV    GOTO :NOT_ACTIVATED
+IF %CONDA_DEFAULT_ENV%==base        GOTO :NOT_ACTIVATED
+
+SET activate_D=%CONDA_PREFIX%\etc\conda\activate.d
+SET working_dir=%activate_D%\set_working_directory.bat
+
+MKDIR %activate_D%
+
+ECHO @ECHO OFF                                                  > %working_dir%
+ECHO REM Auto-CD drive and folder on CONDA Activate             >> %working_dir%
+ECHO REM https://github.com/dvesic/perfect-python-4-windows     >> %working_dir%
+ECHO.           >> %working_dir%
+ECHO %CD:~0,2%  >> %working_dir%
+ECHO CD %CD%    >> %working_dir%
+
+GOTO :END
+
+:NOT_ACTIVATED
+ECHO.
+ECHO Please Activate Environment BEFORE running %~n0
+ECHO.
+
+GOTO :END
+
+:END
+
+
+
+
--- bin/py.cmd
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-REM Toolbox generated shell script for running current PyCharm version
-CALL ..\Scripts\h-pycharm.cmd .
